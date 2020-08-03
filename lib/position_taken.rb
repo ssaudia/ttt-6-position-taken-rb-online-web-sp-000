4a1025dcd141f5b289a71f228abcf390e2d83b79
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-03 00:52:38 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,6 @@
-# code your #position_taken? method here!
+require_relative '../lib/position_taken.rb'
+def position_taken?
+  expect{position_taken?(board, index)}.to_not raise_error
+  board = []
+  index = 0
+end
