83603194f4fda48dfcb9e50c88918c1a9f86d781
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-03 23:01:58 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,3 @@
-require_relative '../lib/position_taken.rb'
-def position_taken?
-  expect{position_taken?(board, index)}.to_not raise_error
-  board = []
-  index = 0
+def position_taken?(board, index)
+  board[index]
 end
