0362cbdd7cd1c2d961d5cdab704b485ca14e974b
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-08-04 00:32:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,8 @@
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
-  board[index]
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
 end
